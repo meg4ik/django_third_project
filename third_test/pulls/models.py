04eb969b197d82bc88339ebca0fef9cf7a9caab3
meg4ik <meg4mag@gmail.com>
2021-02-14 18:51:13 +0200

    more models

---
+++ third_test/pulls/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.utils import timezone
+
+import datetime
+
+# Create your models here.
+
+class Questions(models.Model):
+    questions_text = models.CharField(max_length=100)
+    pub_date = models.DateTimeField('some date')
+
+    def __str__(self):
+        return self.questions_text
+
+    def was_published_recently(self):
+        return self.pub_date >=timezone.now() - datetime.timedelta(days=1)
+
+class Choise(models.Model):
+    question = models.ForeignKey(Questions, on_delete= models.CASCADE)
+    choise_text = models.CharField(max_length = 100)
+    votes = models.IntegerField(default = 0)
+
+    def __str__(self):
+        return self.choise_text
+
