d5922428ca20de3d20799a95c7479753401d7e4b
meg4ik <meg4mag@gmail.com>
2021-02-15 12:27:05 +0200

    voting system has finished

--- third_test/pulls/admin.py
+++ third_test/pulls/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from .models import Questions
+from .models import Questions, Choise
 
 admin.site.register(Questions)
-
+admin.site.register(Choise)
 # Register your models here.
--- third_test/pulls/views.py
+++ third_test/pulls/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-from .models import Questions
+from .models import Choise, Questions
 
 # Create your views here.
 
@@ -14,7 +16,16 @@ def detail(request, q_id):
     return render(request, 'pulls/detail.html', {'que':que})
 
 def results(request, q_id):
-    return None
+    que = get_object_or_404(Questions, id = q_id)
+    return render(request, 'pulls/results.html', {'que':que})
 
 def vote(request, q_id):
-    return None
+    que = get_object_or_404(Questions, id = q_id)
+    try:
+        selected_choise = que.choise_set.get(pk=request.POST['choice'])
+    except (KeyError, Choise.DoesNotExist):
+        return render(request, 'pulls/detail.html', {'que':que})
+    else:
+        selected_choise.votes +=1
+        selected_choise.save()
+        return HttpResponseRedirect(reverse('pulls:results', args=(que.id,)))
