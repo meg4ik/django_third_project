f4088900471bf7a1305497b9deea10028f94f482
meg4ik <meg4mag@gmail.com>
2021-02-18 12:46:33 +0200

    add more tests

--- third_test/pulls/tests.py
+++ third_test/pulls/tests.py
@@ -6,8 +6,57 @@ import datetime
 
 from django.utils import timezone
 from django.test import TestCase
+from django.urls import reverse
 
 from .models import Questions
+
+
+def create_questions(question_text, days):
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Questions.objects.create(questions_text=question_text, pub_date=time)
+
+class QuestionsIndexViewTests(TestCase):
+
+    def test_no_questions(self):
+        response = self.client.get(reverse('pulls:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No pulls are available.")
+        self.assertQuerysetEqual(response.context['latest_que'], [])
+
+    def test_past_question(self):
+        create_questions("Past question", -30)
+        response = self.client.get(reverse('pulls:index'))
+        self.assertQuerysetEqual(response.context['latest_que'], ['<Question: Past question>'])
+
+    def test_future_question(self):
+        create_questions("Future question", 30)
+        response = self.client.get(reverse('pulls:index'))
+        self.assertContains(response, "No pulls are available.")
+        self.assertQuerysetEqual(response.context['latest_que'], [])
+
+    def test_future_question_and_past_question(self):
+        create_questions("Past question", -30)
+        create_questions("Future question", 30)
+        response = self.client.get(reverse('pulls:index'))
+        self.assertQuerysetEqual(response.context['latest_que'], ['<Question: Past question>'])
+
+    def test_two_past_questions(self):
+        create_questions("Past question 1", -30)
+        create_questions("Future question 2", -5)
+        response = self.client.get(reverse('pulls:index'))
+        self.assertQuerysetEqual(response.context['latest_que'], ['<Question: Past question 1>', '<Question: Past question 2>'])
+
+class QuesionDetailViewTests(TestCase):
+    def test_future_question(self):
+        future_question = create_questions("Future question", 5)
+        response = self.client.get(reverse('pulls:detail', args=(future_question.id,)))
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        past_question = create_questions("Past question", -5)
+        response = self.client.get(reverse('pulls:detail', args=(past_question.id,)))
+        self.assertContains(response, past_question.questions_text)
+
 
 class QuestionModelTests(TestCase):
     def test_was_published_recently_with_future_question(self):
